b35c2a75ca9fa8b6a2cae5fae96763ecce2755c6
foyez <foyezar@gmail.com>
2024-07-25 13:19:27 +0200

    bubule sort

--- codes/sorting/bubble-sort.py
+++ codes/sorting/bubble-sort.py
@@ -1,7 +1,22 @@
-# 
+# push the maximum to the last by adjacent swaps
+# TC: O(n^2), SC: O(1)
 
 def bubbleSort(nums):
-    pass
+    for i in range(len(nums) - 1, -1, -1):
+        didSwap = False
+
+        for j in range(0, i):
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+
+                didSwap = True
+
+        if not didSwap:
+            break
+
+    return nums
 
 l = [5, 2, 3, 1]
-bubbleSort(l)
+print(bubbleSort(l))
