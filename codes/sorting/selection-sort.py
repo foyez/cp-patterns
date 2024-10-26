6717658cd6717f067ed2f2f667d47f576b1d4e59
Kazi Foyez Ahmed <foyezar@gmail.com>
2024-10-26 10:55:24 +0200

    update selection sort

--- codes/sorting/selection-sort.py
+++ codes/sorting/selection-sort.py
@@ -4,11 +4,15 @@
 
 def selectionSort(nums):
     for i in range(len(nums) - 1):
+        min_index = i
+
         for j in range(i, len(nums)):
-            if nums[i] > nums[j]:
-                temp = nums[i]
-                nums[i] = nums[j]
-                nums[j] = temp
+            if nums[min_index] > nums[j]:
+                min_index = j
+
+        temp = nums[min_index]
+        nums[min_index] = nums[i]
+        nums[i] = temp
 
     return nums
 
